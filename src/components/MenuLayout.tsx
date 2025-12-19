30d68aa70d6cf28ab1ca4b2520a6541f8e87abb8
HamdiEA <hamdielabedtn@gmail.com>
2025-12-19 15:16:31 +0100

    Updated code to use manually uploaded video

--- src/components/MenuLayout.tsx
+++ src/components/MenuLayout.tsx
@@ -1,15 +1,19 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import GlobalBackground from "@/components/GlobalBackground";
+import { useSmoothSwipe } from "@/hooks/use-smooth-swipe";
 
 interface MenuLayoutProps {
   children: React.ReactNode;
 }
 
 const MenuLayout = ({ children }: MenuLayoutProps) => {
+  // Enable swipe-left to return to the main menu list
+  const { getSwipeStyle } = useSmoothSwipe({ prevPage: "/menu" });
+
   return (
     <GlobalBackground>
-      <div className="flex flex-col flex-1">
+      <div className="flex flex-col flex-1" style={getSwipeStyle()}>
         <Header />
         <div className="flex-1 flex flex-col pt-20">
           <div className="flex-1">
